739bf78b70dcbb61d7a1801b58fb202e1068ba54
Aditya Bansal <bansaladitya209@gmail.com>
2017-02-04 23:33:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//change the text of main-tect div
 var element = document.getElementById('main-text');
 element.innerHTML = 'Aditya Bansal';
+
+//move the image
+var img = document.getElementById('madi');
+img.onclick = function(){
+  img.style.marginLeft = '100px';  
+};
