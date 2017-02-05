1edf23209783275abc685db24086a6eb8dbac916
Aditya Bansal <bansaladitya209@gmail.com>
2017-02-06 00:36:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,6 +113,13 @@ app.get('/counter', function (req,res){
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name',function(req,res){
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName',function(req, res){
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -126,12 +133,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var names = [];
-app.get('/submit-name',function(req,res){
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
